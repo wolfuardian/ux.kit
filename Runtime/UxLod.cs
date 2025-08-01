3cd8e5462118d332dd7a63d39c1eaed5c7aa3781
Eos Wolfuardian <wolfuardian@gmail.com>
2025-08-01 16:12:32 +0800

    feat: 新增 UxLod 的功能用於依據攝影機 FOV 改變縮放

---
+++ Runtime/UxLod.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Ux.Kit
+{
+    [AddComponentMenu("UX Kit/UX LOD")]
+    public class UxLod : MonoBehaviour
+    {
+        [SerializeField] private List<LodSettings> _lodSettings;
+        [SerializeField] private float _currentFov;
+
+        private Camera _camera;
+        private Camera cachedCamera => _camera ??= Camera.main;
+
+        private void Start()
+        {
+            _lodSettings.Sort((a, b) => a._zoom.CompareTo(b._zoom));
+        }
+
+        private void Update()
+        {
+            _currentFov = cachedCamera.fieldOfView;
+        }
+
+        private void LateUpdate()
+        {
+            foreach (var settings in _lodSettings.Where(settings => settings._zoom <= _currentFov))
+            {
+                transform.localScale = Vector3.one * settings._scale;
+            }
+        }
+
+        [Serializable]
+        private class LodSettings
+        {
+            [Header("If Greater Than")]
+            public float _zoom;
+            public float _scale;
+        }
+    }
+}
